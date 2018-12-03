ba038835f7f07da716d354968c201b55508312d2
Cihan Ceyhan <chncyhn@gmail.com>
2018-12-03 20:35:16 +0100

    Day 3 genericified.

--- day_3.py
+++ day_3.py
@@ -1,18 +1,21 @@
+from collections import defaultdict
+
+
 def part_1(claims):
-    fabric = [[0] * 1000 for _ in range(1000)]
+    fabric = defaultdict(int)
 
     for claim in claims:
         _, x, y, w, h = map(int, claim)
         for i in range(x, x + w):
             for j in range(y, y + h):
-                fabric[i][j] += 1
-    return sum(val > 1 for row in fabric for val in row), fabric
+                fabric[(i, j)] += 1
+    return sum(val > 1 for val in fabric.values()), fabric
 
 
 def part_2(fabric, claims):
     for claim in claims:
         _id, x, y, w, h = map(int, claim)
-        if all(fabric[i][j] == 1 for i in range(x, x + w) for j in range(y, y + h)):
+        if all(fabric[(i, j)] == 1 for i in range(x, x + w) for j in range(y, y + h)):
             return _id
     return -1
 
